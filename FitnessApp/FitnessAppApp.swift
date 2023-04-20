2b4a4bbf9b792d1216ce19836cad5873a2fba695
tdelgizzi <tdelgizzi@gmail.com>
2023-04-20 14:51:15 -0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Theo Delgizzi on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
